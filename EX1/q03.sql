4c76f1c8989398a65682411de5793cc472d7510c
= <=>
2017-11-27 22:05:32 +0200

    fixed q03

--- EX1/q03.sql
+++ EX1/q03.sql
@@ -1,29 +1,39 @@
-SELECT 
-    CONCAT(first_name, ' ', last_name)
-FROM
-    actor
-        JOIN
-    (SELECT 
-        actor_id
-    FROM
-        film_actor fa
-    GROUP BY actor_id
-    HAVING COUNT(fa.film_id) >= ALL (SELECT 
-            COUNT(film_actor.film_id)
-        FROM
-            film_actor
-        JOIN (SELECT 
-            film_id
-        FROM
-            inventory
-        JOIN (SELECT DISTINCT
-            inventory_id
-        FROM
-            rental
-        WHERE
-            rental_date >= '2005-06-01'
-                AND rental_date <= '2005-06-30') AS ren ON inventory.inventory_id = ren.inventory_id
-        GROUP BY film_id) AS inv ON film_actor.film_id = inv.film_id
-        GROUP BY actor_id)) AS fTOa ON actor.actor_id = fTOa.actor_id
-
-
+SELECT	CONCAT(first_name, ' ', last_name)
+FROM	actor
+JOIN
+(
+	SELECT actor_idactor_id
+		FROM film_actor
+		JOIN 
+		(
+			SELECT DISTINCT(film_id)
+		        FROM	inventory
+		        JOIN 
+			(
+				SELECT DISTINCT
+				inventory_id
+			        FROM rental
+			        WHERE rental_date >= '2005-06-01' AND rental_date <= '2005-06-30'
+			) AS ren ON inventory.inventory_id = ren.inventory_id
+		) AS inv ON film_actor.film_id = inv.film_id
+	GROUP BY actor_id
+	HAVING COUNT(film_actor.film_id) >= ALL
+	(
+		SELECT COUNT(film_actor.film_id)
+		FROM film_actor
+		JOIN 
+		(
+			SELECT DISTINCT(film_id)
+		        FROM	inventory
+		        JOIN 
+			(
+				SELECT DISTINCT
+				inventory_id
+			        FROM rental
+			        WHERE rental_date >= '2005-06-01' AND rental_date <= '2005-06-30'
+			) AS ren ON inventory.inventory_id = ren.inventory_id
+		) AS inv ON film_actor.film_id = inv.film_id
+	GROUP BY actor_id
+	)
+) AS fTOa
+ON actor.actor_id = fTOa.actor_id
